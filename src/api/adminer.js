7aee42f303c93ce4ab389943649582fe46605f05
fengpengyuan <fengpengyuan@live.com>
2019-03-05 00:24:44 +0800

    bug修复

--- src/api/adminer.js
+++ src/api/adminer.js
@@ -22,3 +22,25 @@ export function getAllRole() {
     method: 'get'
   })
 }
+
+export function getAdminerRoles(id) {
+  return request({
+    url: '/api/getAdminerRoles/' + id,
+    method: 'get',
+  })
+}
+
+export function deleteAdminer(id) {
+  return request({
+    url: '/api/adminers/' + id,
+    method: 'delete',
+  })
+}
+
+export function updateAdminer(data) {
+  return request({
+    url: '/api/adminers/' + data.id,
+    method: 'put',
+    data
+  })
+}
--- src/views/permission/adminer.vue
+++ src/views/permission/adminer.vue
@@ -2,41 +2,46 @@
   <div class="app-container">
     <div class="filter-container">
       <el-button class="filter-item" plain icon="el-icon-plus" @click="add">{{ $t('table.add') }}</el-button>
-      <el-input :placeholder="$t('table.title')" v-model="listQuery.title" style="width: 160px;" class="filter-item"
-        @keyup.enter.native="handleFilter" />
-      <el-date-picker v-model="listQuery.stime" style="width: 160px;" class="filter-item" type="date"
-        placeholder="开始日期" />
-      <el-date-picker v-model="listQuery.etime" style="width: 160px;" class="filter-item" type="date"
-        placeholder="结束日期" />
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+      <el-input :placeholder="$t('table.title')" v-model="query.keyword" style="width: 160px;" class="filter-item"
+        @keyup.enter.native="getData" />
+      <el-date-picker v-model="query.stime" style="width: 160px;" class="filter-item" type="date" placeholder="开始日期" />
+      <el-date-picker v-model="query.etime" style="width: 160px;" class="filter-item" type="date" placeholder="结束日期" />
+      <el-button v-waves class="filter-item" type="primary" :loading="tableLoading" icon="el-icon-search"
+        @click="getData">
         {{ $t('table.search') }}</el-button>
-      <el-button v-waves class="filter-item" plain icon="el-icon-delete" @click="handleClear">
+      <el-button v-waves class="filter-item" plain icon="el-icon-delete" @click="clear">
         {{ $t('table.clear') }}</el-button>
     </div>
-    <el-table v-loading="listLoading" :data="list" highlight-current-row style="width: 100%;" @sort-change="sortChange">
+
+    <el-table v-loading="tableLoading" :data="list" highlight-current-row style="width: 100%;"
+      @sort-change="sortChange">
       <el-table-column label="ID" prop="id" sortable="custom" align="center" width="90" />
-      <el-table-column label="账号" prop="account" align="center" width="110" />
-      <el-table-column label="昵称" prop="nick_name" align="center" width="110" />
-      <el-table-column label="头像" prop="avatar" align="center" width="110" />
-      <el-table-column label="简介" prop="introduction" align="center" width="110" />
+      <el-table-column label="账号" prop="account" align="center" width="200" />
+      <el-table-column label="昵称" prop="nick_name" align="center" width="200" />
+      <el-table-column label="头像" prop="avatar" align="center" width="200">
+        <template slot-scope="scope">
+          <img style="width:50px;height:50px;" :src="scope.row.avatar" alt="" />
+        </template>
+      </el-table-column>
+      <el-table-column label="简介" prop="introduction" align="center" />
       <el-table-column :label="$t('table.actions')" align="center" width="230" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">{{ $t('table.edit') }}
+          <el-button type="primary" size="mini" @click="edit(scope.row)">{{ $t('table.edit') }}
           </el-button>
-          <!-- <el-button v-if="scope.row.status!='published'" size="mini" type="success"
-                        @click="handleModifyStatus(scope.row,'published')">{{ $t('table.publish') }}
-                    </el-button>
-                    <el-button v-if="scope.row.status!='draft'" size="mini"
-                        @click="handleModifyStatus(scope.row,'draft')">{{ $t('table.draft') }}
-                    </el-button>
-                    <el-button v-if="scope.row.status!='deleted'" size="mini" type="danger"
-                        @click="handleModifyStatus(scope.row,'deleted')">{{ $t('table.delete') }}
-                    </el-button> -->
+          <el-popover placement="top" width="160" v-model="scope.row.deletePopover">
+            <p style="text-align:center;padding:3px 0;"><i class="el-icon-warning"></i> 确定要删除吗？</p>
+            <div style="text-align: right; margin: 0">
+              <el-button size="mini" @click="scope.row.deletePopover = false">取消</el-button>
+              <el-button type="danger" size="mini" @click="deleteAdminer(scope.$index)">确定</el-button>
+            </div>
+            <el-button slot="reference" size="mini" type="danger">{{ $t('table.delete') }}
+            </el-button>
+          </el-popover>
         </template>
       </el-table-column>
     </el-table>
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit"
-      @pagination="getList" />
+    <pagination v-show="total>0" :total="total" :page.sync="query.page" :limit.sync="query.limit"
+      @pagination="getData" />
     <Modal v-model="addModal" width="400">
       <p slot="header" style="text-align:center"><span>添加管理员</span></p>
       <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="70px">
@@ -44,7 +49,7 @@
           <el-input @input="inputChange('account')" v-model="ruleForm.account" placeholder="账号" />
           <div v-if="submitErrors.account" class="el-form-item__error">{{ submitErrors.account[0] }}</div>
         </el-form-item>
-        <el-form-item :class="submitErrors.password?'is-error':''" label="密码：" prop="password">
+        <el-form-item v-if="!isEdit" :class="submitErrors.password?'is-error':''" label="密码：" prop="password">
           <el-input @input="inputChange('password')" v-model="ruleForm.password" type="password" placeholder="密码" />
           <div v-if="submitErrors.password" class="el-form-item__error">{{ submitErrors.password[0] }}</div>
         </el-form-item>
@@ -65,7 +70,7 @@
             placeholder="简介" />
         </el-form-item>
         <el-form-item :class="submitErrors.roles?'is-error':''" label="角色：" prop="roles">
-          <el-checkbox-group @input="inputChange('roles')" v-loading="rolesLoading" v-model="ruleForm.roles">
+          <el-checkbox-group v-loading="rolesLoading" v-model="ruleForm.roles">
             <el-checkbox v-for="(role,index) in roles" :label="role.id" name="type">{{ role.name }}
             </el-checkbox>
             <div v-if="submitErrors.roles" class="el-form-item__error">{{ submitErrors.roles[0] }}</div>
@@ -75,14 +80,14 @@
       <div slot="footer">
         <el-button :loading="submitLoading" style="width:76%;" type="primary" @click="submitForm">
           {{ submitLoading ? '提交中': '提交' }}</el-button>
-          <el-button @click="resetForm('ruleForm')" icon="el-icon-refresh">重置</el-button>
+        <el-button @click="resetForm('ruleForm')" icon="el-icon-refresh">重置</el-button>
       </div>
     </Modal>
   </div>
 </template>
 <script>
   import waves from '@/directive/waves'
-  import { fetchList, adminers, getAllRole } from '@/api/adminer'
+  import { fetchList, adminers, getAllRole, getAdminerRoles, deleteAdminer, updateAdminer } from '@/api/adminer'
   import Pagination from '@/components/Pagination'
   import { getToken } from '@/utils/auth'
   export default {
@@ -91,19 +96,21 @@
     components: { Pagination },
     data() {
       return {
-        listQuery: {
-          title: '',
+        deletePopover: false,
+        query: {
+          keyword: '',
           stime: '',
           etime: '',
           page: 1,
           limit: 20
         },
-        listLoading: true,
+        tableLoading: false,
         list: [],
         total: 0,
         keyword: '',
         addModal: false,
         ruleForm: {
+          id: 0,
           account: '',
           nick_name: '',
           introduction: '',
@@ -132,10 +139,11 @@
         rolesLoading: false,
         submitLoading: false,
         submitErrors: {},
+        isEdit: false,
       }
     },
     created() {
-      this.getList()
+      this.getData()
     },
     methods: {
       resetForm() {
@@ -165,25 +173,36 @@
         this.submitErrors = {}
         this.$refs['ruleForm'].resetFields()
       },
-      handleFilter() {
-        this.listQuery.page = 1
-        this.getList()
-      },
-      getList() {
-        fetchList({
-          page: this.page,
-          keyword: this.keyword
-        }).then(res => {
-          this.listLoading = false
+      getData() {
+        this.tableLoading = true
+        fetchList(this.query).then(res => {
+          this.tableLoading = false
           this.list = res.data.list
+          this.list.forEach(item => { item.deletePopover = false })
           this.total = res.data.total
         }).catch(err => {
-          this.listLoading = false
+          this.tableLoading = false
           this.$message.error('服务器或网络错误')
         })
       },
-      handleClear() {
-
+      clear() {
+        this.query.keyword = ''
+        this.query.stime = ''
+        this.query.etime = ''
+        this.query.page = 1
+      },
+      deleteAdminer(k) {
+        deleteAdminer(this.list[k].id).then(res => {
+          if (res.data.code == 0) {
+            this.$message.success('操作成功')
+            this.getData()
+            this.list[k].deletePopover = false
+          } else {
+            this.$message.warning(res.data.msg)
+          }
+        }).catch(err => {
+          this.$message.warning('服务器或网络错误')
+        })
       },
       sortChange(data) {
         const { prop, order } = data
@@ -191,8 +210,29 @@
           this.sortByID(order)
         }
       },
-      handleUpdate(row) {
-
+      edit(row) {
+        this.isEdit = true
+        this.addModal = true
+        this.getAllRole()
+        delete row.created_at
+        delete row.updated_at
+        this.ruleForm.id = row.id
+        this.ruleForm.account = row.account
+        this.ruleForm.nick_name = row.nick_name
+        this.ruleForm.introduction = row.introduction
+        this.ruleForm.avatar = row.avatar
+        this.getAdminerRoles(row.id)
+      },
+      getAdminerRoles(id) {
+        getAdminerRoles(id).then(res => {
+          if (res.data.code == 0) {
+            this.ruleForm.roles = res.data.list
+          } else {
+            this.$message.warning(res.data.msg)
+          }
+        }).catch(err => {
+          this.$message.warning('服务器或网络错误')
+        })
       },
       handleAvatarSuccess(res, file) {
         this.ruleForm.avatar = res.path
@@ -213,23 +253,44 @@
         this.$refs['ruleForm'].validate((valid) => {
           if (valid) {
             this.submitLoading = true
-            adminers(this.ruleForm).then(res => {
-              this.submitLoading = false
-              if (res.data.code == 0) {
-                this.$message.success('操作成功')
-                this.addModal = false
-                this.getList()
-              } else if (res.data.code == 422) {
-                this.oldRuleForm = JSON.parse(JSON.stringify(this.ruleForm))
-                this.submitErrors = res.data.err
-              } else {
-                this.$message.warning(res.data.msg)
-              }
-            }).catch(err => {
-              console.log(err,'err')
-              this.submitLoading = false
-              this.$message.error('服务器或网络错误')
-            })
+            if (this.ruleForm.id == 0) {
+              adminers(this.ruleForm).then(res => {
+                this.submitLoading = false
+                if (res.data.code == 0) {
+                  this.$message.success('操作成功')
+                  this.addModal = false
+                  this.getData()
+                } else if (res.data.code == 422) {
+                  this.oldRuleForm = JSON.parse(JSON.stringify(this.ruleForm))
+                  this.submitErrors = res.data.err
+                } else {
+                  this.$message.warning(res.data.msg)
+                }
+              }).catch(err => {
+                console.log(err, 'err')
+                this.submitLoading = false
+                this.$message.error('服务器或网络错误')
+              })
+            } else {
+              updateAdminer(this.ruleForm).then(res => {
+                this.submitLoading = false
+                if (res.data.code == 0) {
+                  this.$message.success('操作成功')
+                  this.addModal = false
+                  this.getData()
+                } else if (res.data.code == 422) {
+                  this.oldRuleForm = JSON.parse(JSON.stringify(this.ruleForm))
+                  this.submitErrors = res.data.err
+                } else {
+                  this.$message.warning(res.data.msg)
+                }
+              }).catch(err => {
+                console.log(err, 'err')
+                this.submitLoading = false
+                this.$message.error('服务器或网络错误')
+              })
+            }
+
           } else {
             console.log('error submit!!')
             return false
@@ -266,7 +327,8 @@
     height: 100px;
     display: block;
   }
-  .el-form-item.is-error .el-input__inner{
+
+  .el-form-item.is-error .el-input__inner {
     border-color: #f56c6c !important;
   }
 </style>
@@ -276,6 +338,7 @@
     width: 130px;
     overflow: hidden;
   }
+
   .el-checkbox:first-child {
     margin-left: 12px;
   }
